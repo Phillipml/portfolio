d75fc8b26df3547efba14ea6eee41dc8a1d7421b
Phillipml <contato.phillip.menezes@gmail.com>
2025-10-21 12:41:40 -0300

    starts colors

--- src/components/ui/HomeBackground.tsx
+++ src/components/ui/HomeBackground.tsx
@@ -56,8 +56,8 @@ const HomeBackground = () => {
             key={star.id}
             className={`absolute rounded-full ${
               isDarkTheme
-                ? 'bg-tertiary shadow-[0_0_6px_#ffffff]'
-                : 'bg-secondary shadow-[0_0_6px_#001a19]'
+                ? 'bg-secondary shadow-[0_0_6px_#ffffff]'
+                : 'bg-tertiary shadow-[0_0_6px_#001a19]'
             }`}
             style={{
               width: `${star.size}px`,
